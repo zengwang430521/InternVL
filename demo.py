4b83cedcfb1c2be41b2f4b912c8607de38443e2c
zengwang <zengwang@tetras.ai>
2024-06-26 20:57:30 +0800

    demo

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 from lmdeploy import pipeline
 from lmdeploy.vl import load_image
-pipe = pipeline('OpenGVLab/InternVL-Chat-V1-5')
+pipe = pipeline('/mnt/cachenew2/share_data/mayunhe/huggingface/hub/models--OpenGVLab--InternVL-Chat-V1-5/snapshots/c1987c574e0010d8104c545131f97beeffb96a73')
 image = load_image('examples/image2.jpg')
 response = pipe(('describe this image', image))
 print(response)
