05515b188d560d5b3e5677c70f05d764a74b9976
zengwang <zengwang@tetras.ai>
2024-07-12 20:14:32 +0800

    debug

--- cmd_test.sh
+++ cmd_test.sh
@@ -1,6 +1,27 @@
+srun -p pat_taurus -w SH-IDC1-10-142-5-138  --job-name=internvl  --ntasks=1 --gres=gpu:1 --ntasks-per-node=1 \
+/mnt/lustre/zengwang/anaconda3/envs/internvl/bin/lmdeploy  serve api_server \
+/mnt/lustrenew/share_data/zengwang/pretrained_model/InternVL2-26B/InternVL-Chat-V1-5 --model-name InternVL-Chat-V1-5
 
-srun -p pat_taurus -N 1 -w SH-IDC1-10-142-5-127 --gres=gpu:1\
+/mnt/lustrenew/share_data/zengwang/pretrained_model/InternVL-Chat-V1-5 --model-name InternVL-Chat-V1-5
+
+/mnt/lustrenew/share_data/zengwang/pretrained_model/InternVL2_26B --model-name InternVL2_26B
+
+
+/mnt/lustrenew/share_data/zengwang/pretrained_model/InternVL2_40B --model-name InternVL2_40B
+
+/mnt/cache/zengwang/codes/llm/internvl/internvl_chat/work_dirs/agent_pretrain_v1/models--OpenGVLab--InternVL-Chat-V1-5
+
+
+
+ /mnt/cachenew2/share_data/mayunhe/huggingface/hub/models--OpenGVLab--InternVL-Chat-V1-5/snapshots/c1987c574e0010d8104c545131f97beeffb96a73 \
+ --tp 8 --session-len 16384
+
+
+
+srun -p pat_taurus -N 1 --gres=gpu:1\
  /mnt/cachenew2/mayunhe/envs/llm_env/bin/lmdeploy serve api_server \
+ /mnt/cache/zengwang/codes/llm/internvl/internvl_chat/work_dirs/agent_pretrain_v1/checkpoint-1800
+
  /cachenew2/share_data/mayunhe/huggingface/hub/models--OpenGVLab--InternVL-Chat-V1-5/snapshots/c1987c574e0010d8104c545131f97beeffb96a73
 
 
@@ -19,8 +40,8 @@ srun -p pat_taurus -w SH-IDC1-10-142-5-155 --job-name=agent --ntasks=1 --gres=gp
  srun -p pat_taurus -N 1 -w SH-IDC1-10-142-5-127 --gres=gpu:1 python demo.py
 
 
-ssh -N -f -L 2346:10.142.4.32:22 -vvv zengwang@jump-vscode.sensetime.com
-ssh -N -f -L 0.0.0.0:2348:10.142.5.88:23333 dev_1424
+ssh -N -f -L 2346:10.142.4.32:22 zengwang@jump-vscode.sensetime.com
+ssh -N -f -L 0.0.0.0:2348:10.142.5.138:23333 dev_1424
 lsof -i :2348
 
 10.155.176.70
--- demo.py
+++ demo.py
@@ -4,3 +4,17 @@ pipe = pipeline('/mnt/cachenew2/share_data/mayunhe/huggingface/hub/models--OpenG
 image = load_image('examples/image2.jpg')
 response = pipe(('describe this image', image))
 print(response)
+
+
+{
+    "model_name": "internlm2-chat",
+    "system": "你是由上海人工智能实验室联合商汤科技开发的书生多模态大模型，英文名叫InternVL, 是一个有用无害的人工智能助手。",
+    "eosys": "<|im_end|>\n",
+    "user": "<|im_start|>user\n",
+    "eoh": "<|im_end|>\n",
+    "assistant": "<|im_start|>assistant\n",
+    "eoa": "<|im_end|>",
+    "separator": "\n",
+    "capability": "chat",
+    "stop_words": ["<|im_end|>"]
+}
