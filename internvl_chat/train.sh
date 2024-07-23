c9846ca12ab916e1f9ff3c13d6ad1db50fc5c80a
zengwang <zengwang@tetras.ai>
2024-07-23 14:33:40 +0800

    debug

--- internvl_chat/train.sh
+++ internvl_chat/train.sh
@@ -1,6 +1,9 @@
 # using 8 GPUs with slurm system, fine-tune the full LLM
 PER_DEVICE_BATCH_SIZE=2 QUOTA_TYPE='spot' PARTITION='pat_taurus' GPUS=8 sh shell/internlm2_1_8b_dynamic/info_extract_v2.sh
 
+PER_DEVICE_BATCH_SIZE=4 PARTITION='pat_taurus' GPUS=32 sh shell/internlm2_1_8b_dynamic/info_extract_v2.sh
+
+
 PARTITION='pat_taurus' GPUS=32 sh shell/internlm2_20b_dynamic/agent_pretrain_v3.sh
 
 # using 2 GPUs, fine-tune the LoRA
