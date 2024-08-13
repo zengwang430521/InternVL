cdf4f0a58643d636865f2cc7ae63040359c7a2d8
zengwang <zengwang@tetras.ai>
2024-08-13 16:23:18 +0800

    debug

--- internvl_chat/train.sh
+++ internvl_chat/train.sh
@@ -1,5 +1,7 @@
 # using 8 GPUs with slurm system, fine-tune the full LLM
 cd /mnt/mobile/zengwang/code/agent/InternVL/internvl_chat
+PER_DEVICE_BATCH_SIZE=4 PARTITION='pat_taurus' GPUS=8 sh shell/internlm2_1_8b_dynamic/single_op_v1.sh
+
 PER_DEVICE_BATCH_SIZE=2 PARTITION='pat_taurus' GPUS=16 sh shell/internlm2_1_8b_dynamic/info_extract_v10.sh
 
 PER_DEVICE_BATCH_SIZE=1 PARTITION='pat_taurus' GPUS=16 sh shell/internlm2_20b_dynamic/info_extract_v8.sh
