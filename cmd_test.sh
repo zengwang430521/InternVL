e91294841a34d47a3f01da3f5c051f2e21f0dee2
zengwang <zengwang@tetras.ai>
2024-08-22 21:04:58 +0800

    debug

--- cmd_test.sh
+++ cmd_test.sh
@@ -1,9 +1,14 @@
 srun -p pat_taurus  --job-name=internvl  --ntasks=1 --gres=gpu:8 --ntasks-per-node=1 \
 /mnt/lustre/zengwang/anaconda3/envs/internvl/bin/lmdeploy  serve api_server \
+/mnt/lustrenew/share_data/zengwang/pretrained_model/InternVL2-40B --tp 8 --session-len 16384
 /mnt/lustrenew/share_data/zengwang/gui-mobile/models/models--OpenGVLab--InternVL-Chat-V1-5  --tp 8 --session-len 16384
 
 srun -p pat_taurus  -x SH-IDC1-10-142-5-121 --job-name=internvl  --ntasks=1 --gres=gpu:1 --ntasks-per-node=1 \
 /mnt/lustre/zengwang/anaconda3/envs/internvl/bin/lmdeploy  serve api_server \
+/mnt/cache/zengwang/codes/llm/internvl/internvl_chat/work_dirs/single_op_v17/InternVL2-2B --model-name InternVL2-2B   --session-len 8192
+
+/mnt/lustrenew/share_data/zengwang/pretrained_model/InternVL2-40B --tp 4
+
 /mnt/cache/zengwang/codes/llm/internvl/internvl_chat/work_dirs/info_extract_2b_v13/InternVL2-2B --model-name InternVL2-2B
 
 /mnt/cache/zengwang/codes/llm/internvl/internvl_chat/work_dirs/single_op_v11/InternVL2-2B --model-name InternVL2-2B   --session-len 8192
@@ -99,6 +104,62 @@ llm-ckpt.aoss-internal.cn-sh-01.sensecoreapi-oss.cn/internvl2_2b ./
 
 /mnt/mobile/zengwang/ads-cli cp s3://AB570E1C87C643D2A8E9BDD8EC5F12A7:AC3D7623BF464791B930A1CB83E500B1@\
 llm-ckpt.aoss-internal.cn-sh-01.sensecoreapi-oss.cn/screenshot_dataset/info_extract ./
+
+/mnt/mobile/zengwang/ads-cli cp \
+s3://AB570E1C87C643D2A8E9BDD8EC5F12A7:AC3D7623BF464791B930A1CB83E500B1@\
+llm-ckpt.aoss-internal.cn-sh-01.sensecoreapi-oss.cn/screenshot_dataset/screenshot_dataset
+
+/mnt/mobile/zengwang/ads-cli cp \
+s3://953C5A89F16D48FBBBC01CC0676E7B8A:B76EF8EC59D54DA995B88644719B3EDD@\
+foundation-share.aoss-internal.cn-sh-01.sensecoreapi-oss.cn/audio/audio_share ./
+
+
+/mnt/mobile/zengwang/ads-cli cp \
+s3://AB570E1C87C643D2A8E9BDD8EC5F12A7:AC3D7623BF464791B930A1CB83E500B1@\
+foundation-share.aoss-internal.cn-sh-01.sensecoreapi-oss.cn/audio/audio_share ./
+
+/mnt/mobile/zengwang/ads-cli cp \
+s3://953C5A89F16D48FBBBC01CC0676E7B8A:B76EF8EC59D54DA995B88644719B3EDD@\
+foundation-share.aoss-internal.cn-sh-01.sensecoreapi-oss.cn/audio/audio_share ./
+
+
+/mnt/mobile/zengwang/ads-cli cp \
+s3://953C5A89F16D48FBBBC01CC0676E7B8A:B76EF8EC59D54DA995B88644719B3EDD@\
+foundation-share.aoss-internal.cn-sh-01.sensecoreapi-oss.cn/audio/ckpt/d240723_internvl2_2b_audio_stage3 ./
+
+/mnt/mobile/zengwang/ads-cli cp \
+s3://AB570E1C87C643D2A8E9BDD8EC5F12A7:AC3D7623BF464791B930A1CB83E500B1@\
+foundation-share.aoss-internal.cn-sh-01.sensecoreapi-oss.cn/audio/ckpt/d240723_internvl2_2b_audio_stage3 ./
+
+/mnt/mobile/zengwang/ads-cli cp \
+s3://AB570E1C87C643D2A8E9BDD8EC5F12A7:AC3D7623BF464791B930A1CB83E500B1@\
+foundation-share.aoss-internal.cn-sh-01.sensecoreapi-oss.cn/audio/audio_share ./
+
+AccessKey ID： CE4B872D623E4F45B6F7E439B2710959    AccessKey Secret：9433144DEFFD434198393FB186296070
+
+/mnt/mobile/zengwang/ads-cli cp \
+s3://CE4B872D623E4F45B6F7E439B2710959:9433144DEFFD434198393FB186296070@\
+foundation-share.aoss-internal.cn-sh-01.sensecoreapi-oss.cn/audio/audio-share ./
+
+/mnt/mobile/zengwang/ads-cli cp \
+s3://CE4B872D623E4F45B6F7E439B2710959:9433144DEFFD434198393FB186296070@\
+foundation-share.aoss-internal.cn-sh-01.sensecoreapi-oss.cn/audio/ckpt/d240723_internvl2_2b_audio_stage3 ./
+
+/mnt/mobile/zengwang/ads-cli sync d240723_internvl2_2b_audio_stage3 \
+s3://4CBCB7F4FF274CBD9E14ACEFC51FFF89:632F6C6C8F3F46FB90C5B265F75E10F7@llm-results.aoss.st-sh-01.sensecoreapi-oss.cn/ckpt/d240723_internvl2_2b_audio_stage3
+
+/mnt/mobile/zengwang/ads-cli cp s3://AB570E1C87C643D2A8E9BDD8EC5F12A7:AC3D7623BF464791B930A1CB83E500B1@\
+llm-ckpt.aoss-internal.cn-sh-01.sensecoreapi-oss.cn/results ./
+
+
+aws s3 ls  s3://foundation-share --endpoint-url=http://aoss-internal.cn-sh-01.sensecoreapi-oss.cn
+aws s3 ls  s3://llm-ckpt --endpoint-url=http://aoss-internal.cn-sh-01.sensecoreapi-oss.cn
+
+
+
+~/ads-cli cp s3://953C5A89F16D48FBBBC01CC0676E7B8A:B76EF8EC59D54DA995B88644719B3EDD@foundation-share.aoss-internal.cn-sh-01.sensecoreapi-oss.cn/audio/audio_share/ ./
+
+
 
 
 ssh -N -f -L 2346:10.142.4.32:22 zengwang@jump-vscode.sensetime.com
