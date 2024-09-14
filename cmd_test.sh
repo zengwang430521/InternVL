7c56f1b6b058ec3b6fc45db7780dd676eca487fe
zengwang <zengwang@tetras.ai>
2024-09-14 16:19:46 +0800

    debug

--- cmd_test.sh
+++ cmd_test.sh
@@ -9,7 +9,7 @@ srun -p pat_taurus --job-name=internvl  --ntasks=1 --gres=gpu:8 --ntasks-per-nod
 
 srun -p pat_taurus  -w SH-IDC1-10-142-5-89 --job-name=internvl  --ntasks=1 --gres=gpu:1 --ntasks-per-node=1 \
 /mnt/lustre/zengwang/anaconda3/envs/internvl/bin/lmdeploy  serve api_server \
-/mnt/cache/zengwang/codes/llm/internvl/internvl_chat/work_dirs/single_op_v32/InternVL2-2B --model-name InternVL2-2B
+/mnt/cache/zengwang/codes/llm/internvl/internvl_chat/work_dirs/single_op_v35/InternVL2-2B --model-name InternVL2-2B
 
 /mnt/cache/zengwang/codes/llm/internvl/internvl_chat/work_dirs/single_op_v30/InternVL2-2B --model-name InternVL2-2B
 /mnt/lustrenew/share_data/zengwang/pretrained_model/InternVL2-4B --model-name InternVL2-4B
